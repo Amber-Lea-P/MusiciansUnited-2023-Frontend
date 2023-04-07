8f2d9602afa4dae7714a6bdcad5857e97aea334b
Amber Pöntinen <112793091+Amber-Lea-P@users.noreply.github.com>
2023-04-07 11:54:51 +0300

    Delete src directory

--- src/utils/newRequest.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const newRequest = axios.create({
-    baseURL: "https://musiciansunited-backend.onrender.com",
-    withCredentials: true,
-  });
-  
-  export default newRequest;

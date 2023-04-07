d58b11267c3ceca323d69df365d6a062cb11c3b7
Amber Pöntinen <112793091+Amber-Lea-P@users.noreply.github.com>
2023-04-07 13:16:53 +0300

    updating back end information

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "https://musiciansunited-backend.onrender.com",
+    baseURL: "https://musiciansunited.herokuapp.com/",
     withCredentials: true,
   });
 

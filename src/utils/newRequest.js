6662eb7b519b6b716d8ff26d5390527a536dcb33
Amber Pöntinen <112793091+Amber-Lea-P@users.noreply.github.com>
2023-04-07 19:55:04 +0300

    updating baseURL

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "https://musiciansunited-backend.onrender.com",
+    baseURL: "https://musiciansunited-backend.onrender.com/api/",
     withCredentials: true,
   });
-
   
   export default newRequest;
